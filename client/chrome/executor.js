2dd08e8ee0b7138e2fa3363a9a98ac113d465366
whoknowsjas <whoknowsjas@gmail.com>
2012-02-15 15:04:56 -0500

    updated client scripts

--- client/chrome/executor.js
+++ client/chrome/executor.js
@@ -3,33 +3,15 @@ var dbName = "SEMSURF12345test";
 var pKey = dbName+"-"+ver;
 var url = document.URL;
 var key = pKey+"-"+url;
-var page = {
-    url: document.URL,
-    metaMap: getMap(['meta','title'], "")
-};
-
-
-chrome.extension.sendRequest({do: "save", key: key, value: page});
-
-
-//returns a map of elements found
+var page = { url: document.URL, metaMap: getMap(['meta','title'], "") };
+chrome.extension.sendRequest({do: "c_save", key: key, value: page});
 function getMap(tags, m){
-		var map = {};    
-		for(var i in tags){
-			var cTag = tags[i];
-			var md = document.getElementsByTagName(cTag);
-			for(var ii in md){
-				var currMD = md[ii];
-				if(null == currMD) continue;
-				if(undefined != currMD.content && "" != currMD.content){
-					//alert("Name: "+currMD.name+",\r\n Value: "+currMD.content);
-					map[currMD.name] = currMD.content;
-				}else if(undefined != currMD.innerHTML && "" != currMD.innerHTML){
-					//alert("Name: "+currMD.nodeName+",\r\n Value: "+currMD.innerHTML);
-					map[currMD.nodeName] = currMD.innerHTML;
-				}
-			}
-    }
-		return map;
+	var map = {};    
+	for(var i in tags){	var cTag = tags[i];	var md = document.getElementsByTagName(cTag);
+		for(var ii in md){	var currMD = md[ii]; if(null == currMD) continue;
+			if(undefined != currMD.content && "" != currMD.content) map[currMD.name.toLowerCase()] = currMD.content;
+			else if(undefined != currMD.innerHTML && "" != currMD.innerHTML) map[currMD.nodeName.toLowerCase()] = currMD.innerHTML;				
+		}
+    } return map; 
 };
 
