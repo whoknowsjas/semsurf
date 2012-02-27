621ad6badfc387b236e6964b3a2a592026286b43
whoknowsjas <whoknowsjas@gmail.com>
2012-02-26 19:35:09 -0500

    remove dead file

--- server/src/main/java/com/ost/semsurf/repository/GraphRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ost.semsurf.repository;
-
-import com.ost.semsurf.domain.User;
-
-public interface GraphRepository extends org.springframework.data.neo4j.repository.GraphRepository<User> {
-
-}
