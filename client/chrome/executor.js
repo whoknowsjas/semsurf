e088adf0fe7056963b6d5bcacc95838ba30a8669
whoknowsjas <whoknowsjas@gmail.com>
2012-02-27 19:33:47 -0500

    modify client and collection

--- client/chrome/executor.js
+++ client/chrome/executor.js
@@ -1,20 +1,40 @@
-var ver = "1";
-var dbName = "SEMSURF12345test";
-var pKey = dbName+"-"+ver;
-var url = document.URL;
-var key = pKey+"-"+url;
-//var page = { url: document.URL, metaMap: getMap(['meta','title'], "") };
-var page = { url: document.URL, attributes: getMap(['meta','title'], "") };
-chrome.extension.sendRequest({do: "c_save", key: key, value: page});
-function getMap(tags, m){
-	var map = [];    
-	for(var i in tags){	var cTag = tags[i];	var md = document.getElementsByTagName(cTag);
-		for(var ii in md){	var currMD = md[ii]; if(null == currMD) continue;
-			//if(undefined != currMD.content && "" != currMD.content) map[currMD.name.toLowerCase()] = currMD.content;
-			//else if(undefined != currMD.innerHTML && "" != currMD.innerHTML) map[currMD.nodeName.toLowerCase()] = currMD.innerHTML;
-		if(undefined != currMD.content && "" != currMD.content) map[ii] = {key:currMD.name.toLowerCase(), value:currMD.content};
-		else if(undefined != currMD.innerHTML && "" != currMD.innerHTML) map[ii] = {key:currMD.nodeName.toLowerCase(), value:currMD.innerHTML};
-		}
-    } return map; 
+chrome.extension.sendRequest({do: 'null'},function(response){
+	var ver = "1";
+	var dbName = "SEMSURF12345test";
+	var pKey = dbName+"-"+ver;
+	var url = document.URL;
+	var key = pKey+"-"+url;
+	var map = [];
+	getMap(response.param2, map);
+	getAttrMap(response.param3, map);
+	var page = { url: document.URL, attributes: map };
+	chrome.extension.sendRequest({do: "c_save", key: key, value: page});
+});
+function getAttrMap(tags, map){
+	for(var i in tags){	
+		var tag = tags[i].split("|")[0];
+		var attr = tags[i].split("|")[1];		
+		var oTags = $(tag+'[name='+attr+']').get();			
+		for(var ii in oTags){
+			var currMD = oTags[ii];
+		  if(null == currMD || undefined == currMD) continue;
+			map[map.length++] = {key:currMD.name.toLowerCase(), value:currMD.content};
+		}	
+  } return map; 
+};
+function getMap(tags, map){
+	for(var i in tags){	
+		var oTags = $(tags[i]).get();		
+		for(var ii in oTags){
+			var currMD = oTags[ii];
+		  if(null == currMD || undefined == currMD) continue;
+			map[map.length++] = {key:currMD.nodeName.toLowerCase(), value:currMD.innerHTML};
+		}	
+  } return map; 
+};
+function filterMap(currMD, metaFilters){
+	for(var filter in metaFilters){
+		if(metaFilters[filter] == currMD.name.toLowerCase())	{ return true; }
+	}return false;
 };
 
